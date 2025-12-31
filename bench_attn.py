8c98b7617993c0079715b28140564f36c8c47c1b
Birch-san <alex@anlatan.ai>
2025-12-31 00:12:10 +0000

    try with cudagraph

--- bench_attn.py
+++ bench_attn.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional
+from typing import Any, Callable, Optional
 import math
 from pathlib import Path
 from dataclasses import dataclass
@@ -11,8 +11,10 @@ from triton.testing import do_bench
 from torch.testing import assert_close
 from torch.profiler import ProfilerActivity, profile
 from src.rope import RopeInPlace
+# import torch._inductor.config
+# torch._inductor.config.triton.cudagraph_trees = False
 
-if use_fa3 := True:
+if use_fa3 := False:
     get_attn_out: Callable[[Tensor|tuple[Tensor, ...]], Tensor]
     if use_local_fa3_kernel := True:
         flash_attn_interface = get_local_kernel(repo_path=Path('hf-kernels/flash-attention-3'), package_name='flash_attention_3').flash_attn_interface
@@ -255,6 +257,65 @@ class CausalSelfAttentionOrig(CausalSelfAttentionBase):
         y = F.linear(y, sa_lambdas[1] * self.qkvo_w[self.dim * 3:].type_as(y))  # sa_lambdas[1] pre-multiplied to O @shenberg
         return y
 
+class CausalSelfAttentionCG(nn.Module):
+    def __init__(self, dim: int, head_dim: int, num_heads: int):
+        super().__init__()
+        self.num_heads = num_heads
+        self.head_dim = head_dim
+        self.dim = dim
+        self.hdim = num_heads * head_dim
+
+        assert self.hdim == self.dim, "num_heads * head_dim must equal model_dim"
+        self.qkv = nn.Linear(out_features=self.dim * 3, in_features=self.hdim, bias=False)
+        self.o = nn.Linear(out_features=self.dim, in_features=self.hdim, bias=False)
+
+        # sparse gated attention to enable context based no-op by @classiclarryd
+        self.attn_gate = CastedLinear(12, num_heads)
+        self.attn_gate.weight.label = 'attn_gate'
+
+    def forward(self, x: Tensor, attn_args: AttnArgs):
+        B, T = x.size(0), x.size(1) # batch size, sequence length
+        assert B == 1, "varlen sequences requires B == 1"
+        assert T % 16 == 0
+        # unpack attention args
+        cos, sin = attn_args.cos, attn_args.sin
+        ve, sa_lambdas, key_shift = attn_args.ve, attn_args.sa_lambdas, attn_args.key_shift
+        seqlens, attn_scale, bm_size = attn_args.seqlens, attn_args.attn_scale, attn_args.bm_size
+
+        q, k, v = F.linear(x, sa_lambdas[0] * self.qkv.weight.type_as(x)).view(B, T, 3 * self.num_heads, self.head_dim).chunk(3, dim=-2)
+        q, k = norm(q), norm(k) # QK norm @Grad62304977
+        q, k = rotary(q, cos, sin), rotary(k, cos, sin)
+        if key_shift:
+            # shift keys forward for the stationary head dims. Enables 1-layer induction.
+            k[:, 1:, :, self.head_dim//4:self.head_dim//2] = k[:, :-1, :, self.head_dim//4:self.head_dim//2]
+            k[:, 1:, :, self.head_dim//4+self.head_dim//2:] = k[:, :-1, :, self.head_dim//4+self.head_dim//2:]
+        if ve is not None:
+            v = v + ve.view_as(v) # @ KoszarskyB & @Grad62304977
+
+        max_len = args.train_max_seq_len if self.training else (args.val_batch_size // (grad_accum_steps * world_size))
+
+        # use flash_attn over flex_attn @varunneal. flash_attn_varlen suggested by @YouJiacheng
+        y: Tensor = varlen_attn(
+            q[0],
+            k[0],
+            v[0],
+            max_seqlen_q=max_len,
+            max_seqlen_k=max_len,
+            cum_seq_q=seqlens,
+            cum_seq_k=seqlens,
+            causal=True,
+            scale=attn_scale,
+            window_size_left=bm_size,
+            window_size_right=0,
+        )
+        y = q
+        y = y.view(B, T, self.num_heads, self.head_dim)
+        y = y * torch.sigmoid(self.attn_gate(x[..., :self.attn_gate.weight.size(-1)])).view(B, T, self.num_heads, 1)
+        y = y.contiguous().view(B, T, self.num_heads * self.head_dim) # re-assemble all head outputs side by side
+        y = F.linear(y, sa_lambdas[1] * self.o.weight.type_as(y))  # sa_lambdas[1] pre-multiplied to O @shenberg
+        y = y.view_as(x)
+        return y
+
 class CausalSelfAttentionNext(CausalSelfAttentionOrig):
     def forward(self, x: Tensor, attn_args: AttnArgs):
         B, T = x.size(0), x.size(1) # batch size, sequence length
@@ -322,8 +383,6 @@ pergpu_minibsz=minibsz//grad_accum_steps
 microbsz = pergpu_minibsz // world_size
 max_num_docs = next_multiple_of_n(microbsz // 300, n=128)
 seqlens=torch.arange(0, avg_seqlen*max_num_docs, avg_seqlen, dtype=torch.int32, device=device).clamp_max_(microbsz)
-ve = torch.randn((microbsz, dim), device=device, dtype=hp_dtype, requires_grad=True)
-sa_lambdas = torch.tensor((.5, 1.), device=device, requires_grad=True)
 
 short_bm=128
 
@@ -339,25 +398,37 @@ with torch.device('meta'):
         head_dim=head_dim,
         num_heads=num_heads,
     )
+    cg = CausalSelfAttentionCG(
+        dim=dim,
+        head_dim=head_dim,
+        num_heads=num_heads,
+    )
 seed=42
 gen=torch.Generator(device)
 loss_fn = nn.MSELoss()
 
-for attn in (orig, next):
+for attn in (orig, next, cg):
     attn.to_empty(device=device)
-    attn.qkvo_w.data.normal_(std=attn.dim**-.5, generator=gen.manual_seed(seed))
+    if attn is not cg:
+        attn.qkvo_w.data.normal_(std=attn.dim**-.5, generator=gen.manual_seed(seed))
     attn.attn_gate.weight.data.normal_(std=attn.attn_gate.in_features**-.5, generator=gen.manual_seed(seed))
 
-orig.forward = torch.compile(orig.forward, dynamic=False, fullgraph=True)
-next.forward = torch.compile(next.forward, dynamic=False, fullgraph=True)
+qkv, o = orig.qkvo_w.tensor_split((orig.dim*3,), dim=-2)
+cg.qkv.weight.data.copy_(qkv)
+cg.o.weight.data.copy_(o)
 
-input = torch.randn((1, microbsz, dim), device=device, dtype=hp_dtype, generator=gen.manual_seed(seed+1), requires_grad=True)
+orig = torch.compile(orig, dynamic=False, fullgraph=True)
+cg = torch.compile(cg, dynamic=False, fullgraph=True, mode='reduce-overhead')
+# next = torch.compile(next, dynamic=False, fullgraph=True, mode='reduce-overhead')
+
 target = torch.randn((1, microbsz, dim), device=device, dtype=hp_dtype, generator=gen.manual_seed(seed+2))
 
 def do_fwd(mod: CausalSelfAttentionBase):
+    ve = torch.randn((microbsz, dim), device=device, dtype=hp_dtype, requires_grad=True)
+    sa_lambdas = torch.tensor((.5, 1.), device=device, requires_grad=True)
     attn_args = AttnArgs(
-        ve=ve.clone(),
-        sa_lambdas=sa_lambdas.clone(),
+        ve=ve,
+        sa_lambdas=sa_lambdas,
         seqlens=seqlens,
         bm_size=short_bm,
         cos=yarn.cos,
@@ -366,7 +437,8 @@ def do_fwd(mod: CausalSelfAttentionBase):
         # attn_scale=head_dim**-.5,
         key_shift=False
     )
-    out: Tensor = mod(input.clone(), attn_args=attn_args)
+    input = torch.randn((1, microbsz, dim), device=device, dtype=hp_dtype, generator=gen.manual_seed(seed+1), requires_grad=True)
+    out: Tensor = mod(input, attn_args=attn_args)
     return out
 
 def do_lossbwd(out: Tensor):
@@ -374,9 +446,19 @@ def do_lossbwd(out: Tensor):
     loss.backward()
     return loss
 
-def do_fwdbwd(mod: CausalSelfAttentionBase):
+def do_fwdbwd(mod: nn.Module):
     out: Tensor = do_fwd(mod)
     do_lossbwd(out)
+
+def with_cudagraph_do_fwd(mod: nn.Module):
+    torch.compiler.cudagraph_mark_step_begin()
+    return do_fwd(mod)
+
+def with_cudagraph_do_fwdbwd(mod: nn.Module):
+    torch.compiler.cudagraph_mark_step_begin()
+    out: Tensor = do_fwd(mod)
+    loss: Tensor = loss_fn(out, target)
+    loss.backward()
 
 if test_correctness := False:
     out_orig = do_fwd(orig)
@@ -391,7 +473,7 @@ if test_correctness := False:
     assert_close(orig.qkvo_w.grad, next.qkvo_w.grad, rtol=1e-6, atol=5e-5)
     assert_close(orig.attn_gate.weight.grad, next.attn_gate.weight.grad)
 
-if do_profile := True:
+if do_profile := False:
     prof = profile(
         activities=[
             ProfilerActivity.CPU,
@@ -405,15 +487,24 @@ if do_profile := True:
         do_fwdbwd(orig)
         with prof:
             do_fwdbwd(orig)
-        trace_dir = Path("out_trace")
+        trace_dir = Path("out_trace_nanogpt")
         trace_dir.mkdir(exist_ok=True)
         profile_path = trace_dir / f"{label}.json"
         print(f"Saving profile to {profile_path}")
         prof.export_chrome_trace(str(profile_path))
 
-if test_latency := False:
-    orig_ms: float = do_bench(partial(do_fwdbwd, mod=orig), rep=9000, warmup=1000)
-    next_ms: float = do_bench(partial(do_fwdbwd, mod=next), rep=9000, warmup=1000)
+if test_fwdbwd := False:
+    with_cudagraph_do_fwdbwd(cg)
+    torch.cuda.synchronize()
+    cg.zero_grad()
+    with_cudagraph_do_fwdbwd(cg)
+    torch.cuda.synchronize()
+
+if test_latency := True:
+    warmup, rep = 25, 100
+    orig_ms: float = do_bench(partial(do_fwdbwd, mod=orig), rep=rep, warmup=warmup)
+    # next_ms: float = do_bench(partial(with_cudagraph_do_fwdbwd, mod=next), rep=rep, warmup=warmup)
+    next_ms: float = do_bench(partial(with_cudagraph_do_fwdbwd, mod=cg), rep=rep, warmup=warmup)
     orig_its: float = 1000 / orig_ms
     next_its: float = 1000 / next_ms
     print(f"""
