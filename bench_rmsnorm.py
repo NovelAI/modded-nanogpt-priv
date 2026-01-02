8532f619f1d8975e54f7a89bb90f4c16bec579cb
Birch-san <alex@anlatan.ai>
2026-01-02 12:55:49 +0000

    RMSNorm benchmark

---
+++ bench_rmsnorm.py
@@ -0,0 +1,217 @@
+# benchmark based on Carson Poole's RMSNorm gist
+# https://gist.github.com/carsonpo/5011a284d54a6c7c872da4b991d449ab
+# with changes by Alex Birch to support profiling, optional compress_py kernel, try out cudagraphs, prefer assert_close and prefer do_bench
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+import torch
+from functools import partial
+from torch.testing import assert_close
+from torch.profiler import ProfilerActivity, profile
+try:
+    import compress_py
+    cpy_found = True
+except ImportError:
+    print("[WARN] compress_py not available on your system")
+    cpy_found = False
+
+#---
+# do_bench is from triton, MIT-licensed
+# https://github.com/triton-lang/triton/blob/11ec6354/python/triton/testing.py#L127
+# with fixes by Alex Birch to clear grads before warmup steps too (cudagraphs models are sensitive to tensor reuse)
+# (in case you wanted to test backwards pass after this)
+
+from triton import runtime
+from triton.testing import _summarize_statistics
+
+def do_bench(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, return_mode="mean"):
+    """
+    Benchmark the runtime of the provided function. By default, return the median runtime of :code:`fn` along with
+    the 20-th and 80-th performance percentile.
+
+    :param fn: Function to benchmark
+    :type fn: Callable
+    :param warmup: Warmup time (in ms)
+    :type warmup: int
+    :param rep: Repetition time (in ms)
+    :type rep: int
+    :param grad_to_none: Reset the gradient of the provided tensor to None
+    :type grad_to_none: torch.tensor, optional
+    :param quantiles: Performance percentile to return in addition to the median.
+    :type quantiles: list[float], optional
+    :param return_mode: The statistical measure to return. Options are "min", "max", "mean", "median", or "all". Default is "mean".
+    :type return_mode: str
+    """
+    assert return_mode in ["min", "max", "mean", "median", "all"]
+
+    di = runtime.driver.active.get_device_interface()
+
+    if grad_to_none is not None:
+        for x in grad_to_none:
+            x.grad = None
+    fn()
+    di.synchronize()
+
+    cache = runtime.driver.active.get_empty_cache_for_benchmark()
+
+    # Estimate the runtime of the function
+    start_event = di.Event(enable_timing=True)
+    end_event = di.Event(enable_timing=True)
+    start_event.record()
+    for _ in range(5):
+        runtime.driver.active.clear_cache(cache)
+        if grad_to_none is not None:
+            for x in grad_to_none:
+                x.grad = None
+        fn()
+    end_event.record()
+    di.synchronize()
+    estimate_ms = start_event.elapsed_time(end_event) / 5
+
+    # compute number of warmup and repeat
+    n_warmup = max(1, int(warmup / estimate_ms))
+    n_repeat = max(1, int(rep / estimate_ms))
+    start_event = [di.Event(enable_timing=True) for i in range(n_repeat)]
+    end_event = [di.Event(enable_timing=True) for i in range(n_repeat)]
+    # Warm-up
+    for _ in range(n_warmup):
+        if grad_to_none is not None:
+            for x in grad_to_none:
+                x.grad = None
+        fn()
+    # Benchmark
+    for i in range(n_repeat):
+        # we don't want `fn` to accumulate gradient values
+        # if it contains a backward pass. So we clear the
+        # provided gradients
+        if grad_to_none is not None:
+            for x in grad_to_none:
+                x.grad = None
+        # we clear the L2 cache before each run
+        runtime.driver.active.clear_cache(cache)
+        # record time of `fn`
+        start_event[i].record()
+        fn()
+        end_event[i].record()
+    # Record clocks
+    di.synchronize()
+    times = [s.elapsed_time(e) for s, e in zip(start_event, end_event)]
+    return _summarize_statistics(times, quantiles, return_mode)
+#---
+
+def rmsnorm_eager(x: torch.Tensor, weight: torch.Tensor) -> torch.Tensor:
+    x_fp32 = x.float()
+    rms = torch.rsqrt(x_fp32.pow(2).mean(-1, keepdim=True) + 1e-5)
+    return (x_fp32 * rms * weight).type_as(x)
+
+
+def rmsnorm_custom(x: torch.Tensor, weight: torch.Tensor) -> torch.Tensor:
+    return compress_py.rmsnorm_forward(x, weight, 1e-5)
+
+
+@torch.compile(mode="max-autotune", dynamic=False, fullgraph=True)
+def compiled_rmsnorm(x: torch.Tensor, weight: torch.Tensor) -> torch.Tensor:
+    x_fp32 = x.float()
+    rms = torch.rsqrt(x_fp32.pow(2).mean(-1, keepdim=True) + 1e-5)
+    return (x_fp32 * rms * weight).type_as(x)
+
+@torch.compile(mode="reduce-overhead", dynamic=False, fullgraph=True)
+def cudagraph_rmsnorm(x: torch.Tensor, weight: torch.Tensor) -> torch.Tensor:
+    x_fp32 = x.float()
+    rms = torch.rsqrt(x_fp32.pow(2).mean(-1, keepdim=True) + 1e-5)
+    return (x_fp32 * rms * weight).type_as(x)
+
+@dataclass
+class BenchResult:
+    ms_per_iter: float
+
+    @property
+    def iter_per_s(self) -> float:
+        return 1000 / self.ms_per_iter
+
+class StrategyName(Enum):
+    Eager = 'eager'
+    Compiled = 'compiled'
+    CudaGraph = 'cudagraph'
+    Custom = 'custom'
+
+def main():
+    device = torch.device('cuda')
+    gen = torch.Generator(device)
+    x = torch.randn((1024, 512), generator=gen.manual_seed(42), device=device).type(torch.float8_e4m3fn)
+    weight = torch.randn((512), generator=gen.manual_seed(43), device=device)
+
+    if check_correctness := False:
+        eager_out = rmsnorm_eager(x, weight)
+        compiled_out = compiled_rmsnorm(x, weight)
+        cudagraph_out = cudagraph_rmsnorm(x, weight)
+        eager_out_float = eager_out.float()
+        assert_close(eager_out_float, compiled_out.float())
+        print("compiled_out is close to eager_out")
+        assert_close(eager_out_float, cudagraph_out.float())
+        print("cudagraph_out is close to eager_out")
+
+        if cpy_found:
+            custom_out = rmsnorm_custom(x, weight)
+            assert_close(eager_out_float, custom_out.float())
+            print("custom_out is close to eager_out")
+
+    if do_profile := False:
+        wait, warmup, active = 1, 1, 1
+        prof_its = wait + warmup + active
+        prof = profile(
+            activities=[
+                ProfilerActivity.CPU,
+                ProfilerActivity.CUDA,
+            ],
+            record_shapes=False,
+            # stack traces introduce sufficient CPU overhead as to mislead, so don't believe such profiles entirely.
+            # with_stack=True,
+            schedule=torch.profiler.schedule(wait=wait, warmup=warmup, active=active),
+        )
+        torch.cuda.synchronize()
+        for fn, label, wants_cudagraph in zip((
+            rmsnorm_eager,
+            compiled_rmsnorm,
+            cudagraph_rmsnorm,
+            *(rmsnorm_custom,) * cpy_found,
+        ), (
+            StrategyName.Eager,
+            StrategyName.Compiled,
+            StrategyName.CudaGraph,
+            *(StrategyName.Custom,) * cpy_found,
+        ), (
+            False,
+            False,
+            True,
+            *(False,) * cpy_found,
+        ), strict=True):
+            with prof:
+                for _ in range(prof_its):
+                    if wants_cudagraph:
+                        torch.compiler.cudagraph_mark_step_begin()
+                    fn(x, weight)
+                    torch.cuda.synchronize()
+                    prof.step()
+            trace_dir = Path("out_trace_rmsnorm")
+            trace_dir.mkdir(exist_ok=True)
+            profile_path = trace_dir / f"{label.value}.json"
+            print(f"Saving profile to {profile_path}")
+            prof.export_chrome_trace(str(profile_path))
+
+    if do_benchmark := True:
+        warmup, rep = 1000, 2000
+        bench_results: dict[StrategyName, BenchResult] = {}
+        bench_results[StrategyName.Eager] = BenchResult(do_bench(partial(rmsnorm_eager, x, weight), rep=rep, warmup=warmup))
+        bench_results[StrategyName.Compiled] = BenchResult(do_bench(partial(compiled_rmsnorm, x, weight), rep=rep, warmup=warmup))
+        bench_results[StrategyName.CudaGraph] = BenchResult(do_bench(partial(cudagraph_rmsnorm, x, weight), rep=rep, warmup=warmup))
+
+        if cpy_found:
+            bench_results[StrategyName.Custom] = BenchResult(do_bench(partial(rmsnorm_custom, x, weight), rep=rep, warmup=warmup))
+
+        bench_result = "\n".join((f"{name.value.rjust(9)}:  {result.ms_per_iter:5.3f}ms  {result.iter_per_s:12.2f}it/s" for name, result in bench_results.items()))
+        print(bench_result)
+
+
+if __name__ == "__main__":
+    main()
